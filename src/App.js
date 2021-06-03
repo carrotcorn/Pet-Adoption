073b8198ab2785264e544eccb588ef6d3528fb8e
Eric Bourne <ericbourne6371@gmail.com>
2021-06-03 14:38:53 -0700

    updated eslint for use of eslint-plugin-react-hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams"
 
 const App = () => {
   return(
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Ash" animal="Dog" breed="Australian Sheppard"/>
-    <Pet name="Tom" animal="Cat" breed="American Short-Hair"/>
-    <Pet name="SxreeDumps" animal="Cat" breed="Calico"/>
+    <SearchParams />
   </div>)
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,23 @@
-const SearchParams = () => {
-   return(
-      <div className="search-params">
+import { useState } from "react";
 
-      </div>
-   )
-}
+const SearchParams = () => {
+  const [location, setLocation] = useState("Vancouver, BC");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 export default SearchParams;
