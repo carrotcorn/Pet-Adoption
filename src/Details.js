eb706a97066b108b506773248b32331e358e3094
Eric Bourne <ericbourne6371@gmail.com>
2021-06-07 15:31:28 -0700

    going over class components and lifecycle methods

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,50 @@
-const Details = () => {
-   return <h2>Hi</h2>
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+    //////// EDUCATIONAL CODE BELOW **********
+    // this.setState({
+    //    loading: false,
+    //    name:json.pets[0].name,
+    //    breed:json.pets[0].breed,
+    //    description:json.pets[0].description,
+    //       //etc. this is the long way of setting state for the object.
+    //       //instead of every value of the object being set one at a time
+    //       //object.assign() does this with just two lines of code
+    // });
+  }
+
+  render() {
+    //   console.log(this.state);
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details
+export default withRouter(Details);
