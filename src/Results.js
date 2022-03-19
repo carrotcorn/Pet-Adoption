8aa480dac5ea097cf39e3c4dcb14de32765b902f
Eric Bourne <ericbourne6371@gmail.com>
2022-03-19 14:59:35 -0700

    repair

--- src/Results.js
+++ src/Results.js
@@ -1,43 +1,66 @@
+import Pet from "./Pet";
 
-import Pet from './Pet'
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
-function Results({pets}){
-   
-   return(
-      <div className="search">
-         {/* {!pets.length ? (
-            <h2>No Pets Found</h2>
-          ) : (
-             pets.map((pet) => (
-                <Pet 
-                animal={pet.animal}
-                key={pet.id}
-                name={pet.name}
-                breed={pet.breed}
-                images={pet.images}
-                location={`${pet.city}, ${pet.state}`}
-                id={pet.id}
-                />
-            ))
-          )
-         } */}
-        {(pet)=>{
-            <Pet 
-               animal={pet.animal}
-               key={pet.id}
-               name={pet.name}
-               breed={pet.breed}
-               images={pet.images}
-               location={`${pet.city}, ${pet.state}`}
-               id={pet.id}
-            />
-         }}
-      </div>
-   )
-}
+export default Results;
 
-export default Results
+// import Pet from "./Pet";
 
+// function Results({ pets }) {
+//   return (
+//     <div className="search">
+//       {/* {!pets.length ? (
+//             <h2>No Pets Found</h2>
+//           ) : (
+//              pets.map((pet) => (
+//                 <Pet
+//                 animal={pet.animal}
+//                 key={pet.id}
+//                 name={pet.name}
+//                 breed={pet.breed}
+//                 images={pet.images}
+//                 location={`${pet.city}, ${pet.state}`}
+//                 id={pet.id}
+//                 />
+//             ))
+//           )
+//          } */}
+//       {(pet) => {
+//         <Pet
+//           animal={pet.animal}
+//           key={pet.id}
+//           name={pet.name}
+//           breed={pet.breed}
+//           images={pet.images}
+//           location={`${pet.city}, ${pet.state}`}
+//           id={pet.id}
+//         />;
+//       }}
+//     </div>
+//   );
+// }
+
+// export default Results;
 
 //   const [offset, setOffset] = useState(0);
 //   const [data, setData] = useState([]);
@@ -60,28 +83,28 @@ export default Results
 //     const selectedPage  = e.selected;
 //     setOffset(selectedPage + 1)
 //   }
-   // const [petData, setPetData] = useState([]);
-   // const [pageNumber, setPageNumber] = useState(0);
-   // // const [pageCount] = useState(Math.ceil(data.meta.total_count / data.meta.limit))
-   // // const [animal] = useState("")
+//    const [petData, setPetData] = useState([]);
+//    const [pageNumber, setPageNumber] = useState(0);
+//    // const [pageCount] = useState(Math.ceil(data.meta.total_count / data.meta.limit))
+//    // const [animal] = useState("")
 
-   // const petsPerPage = 10;
-   // const pagesVisited = pageNumber * petsPerPage;
+//    const petsPerPage = 10;
+//    const pagesVisited = pageNumber * petsPerPage;
 
-   // const displayPets = petData
-   // .slice(pagesVisited, pagesVisited + petsPerPage).map()
+//    const displayPets = petData
+//    .slice(pagesVisited, pagesVisited + petsPerPage).map()
 
-   // useEffect(() => {
-   //    paginatePets();
-   //  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-  
-   // async function paginatePets() {
-   // const res = await fetch(
-   //    `http://pets-v2.dev-apis.com/pets?animal=${animal}&page=${pageNumber}`
-   // );
-   // const json = await res.json();
+//    useEffect(() => {
+//       paginatePets();
+//     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-   // console.log(json);
+//    async function paginatePets() {
+//    const res = await fetch(
+//       `http://pets-v2.dev-apis.com/pets?animal=${animal}&page=${pageNumber}`
+//    );
+//    const json = await res.json();
 
-   // setPetData(json.pets);
-   // }
+//    console.log(json);
+
+//    setPetData(json.pets);
+//    }
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,114 +1,32 @@
 import { useState, useEffect, useContext } from "react";
-import axios from 'axios'
-import ReactPaginate from 'react-paginate';
-// import {Link} from "react-router-dom";
-
 import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
-import Pet from "./Pet"
 import Results from "./Results";
 
 const ANIMALS = ["cat", "dog", "reptile", "bird", "rabbit"];
 
-const SearchParams = (pet) => {
+const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const [petsData, setPets] = useState([]);
+  const [pets, setPets] = useState([]);
   const [breedList] = useBreedList(animal);
   const [theme, setTheme] = useContext(ThemeContext);
 
-  const [pagesVisited, setPagesVisited] = useState(0);
-  const [data, setData] = useState([]);
-  const [perPage] = useState(10);
-  const [pageCount, setPageCount] = useState(0);
-
-
   useEffect(() => {
     requestPets();
-  }, [pagesVisited]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    async function requestPets() {
-      const res = await fetch(
-        `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-      );
-      const json = await res.json();
+  async function requestPets() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
 
-      console.log(json);
+    console.log(json);
 
-      setPets(json.pets);
-
-      const resX = await axios.get(`http://pets-v2.dev-apis.com/pets?animal=${animal}&page=${pageCount}`)
-      const data = resX.data.pets;
-      console.log(data)
-
-        const grabbedPets = data.slice(pagesVisited, pagesVisited + perPage);
-        const postData =  
-          !grabbedPets.length ? (
-            <h2>No Pets Found</h2>
-          ) : (
-            grabbedPets.map((pet) => (
-            // <Results 
-            //   key={pet.id} 
-            //   pets={pet.petsData} 
-            // />
-            
-                <Pet 
-                  animal={pet.animal}
-                  key={pet.id}
-                  name={pet.name}
-                  breed={pet.breed}
-                  images={pet.images}
-                  location={`${pet.city}, ${pet.state}`}
-                  id={pet.id}
-                  />
-                )) 
-             ) 
-
-      setData(postData)
-      setPageCount(Math.ceil(data.length / perPage))
-
-    }
-
-  // useEffect(() => {
-  //   getData()
-  // }, [pagesVisited]); // eslint-disable-line react-hooks/exhaustive-deps
-  
-    // const getData = async() => {
-      // const res = await axios.get(`http://pets-v2.dev-apis.com/pets?animal=${animal}&page=${pageCount}`)
-      // const data = res.data.pets;
-      // console.log(data)
-
-      //   const grabbedPets = data.slice(pagesVisited, pagesVisited + perPage);
-      //   const postData =  
-      //     !grabbedPets.length ? (
-      //       <h2>No Pets Found</h2>
-      //     ) : (
-      //       grabbedPets.map((pet) => (
-      //           <Pet 
-      //           animal={pet.animal}
-      //           key={pet.id}
-      //           name={pet.name}
-      //           breed={pet.breed}
-      //           images={pet.images}
-      //           location={`${pet.city}, ${pet.state}`}
-      //           id={pet.id}
-      //           />
-      //       ))
-      //     )
-       
-      //     // <Results pets={petsData} />
-
-      // setData(postData)
-      // setPageCount(Math.ceil(data.length / perPage))
-    // }
-
-  
-  const handlePageClick = (e) => {
-    const selectedPage  = e.selected
-    setPagesVisited(selectedPage + 1)
+    setPets(json.pets);
   }
-  
 
   return (
     <div className="search-params">
@@ -174,20 +92,7 @@ const SearchParams = (pet) => {
         </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
-      {/* <Results pets={petsData} /> */}
-      {data}
-      <ReactPaginate
-        previousLabel={"prev"}
-        nextLabel={"next"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}/>
+      <Results pets={pets} />
     </div>
   );
 };
